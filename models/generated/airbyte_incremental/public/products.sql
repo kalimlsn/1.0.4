b062dfb9dd6a1e7b1f9f1e488416899e81fa1ccf
Kalimulla Mohammed <mohammed.kalimulla@lsnsoft.in>
2022-12-06 11:14:25 +0530

    first commit

---
+++ models/generated/airbyte_incremental/public/products.sql
@@ -0,0 +1,30 @@
+{{ config(
+    indexes = [{'columns':['_airbyte_unique_key'],'unique':True}],
+    unique_key = "_airbyte_unique_key",
+    schema = "public",
+    tags = [ "top-level" ]
+) }}
+-- Final base SQL model
+-- depends_on: {{ ref('products_scd') }}
+select
+    _airbyte_unique_key,
+    {{ adapter.quote('id') }},
+    sku,
+    {{ adapter.quote('name') }} as title,
+    date_created,
+    date_modified,
+    type,
+    status,
+    description,
+    tags,
+    images,
+    _airbyte_ab_id,
+    _airbyte_emitted_at,
+    {{ current_timestamp() }} as _airbyte_normalized_at,
+    _airbyte_products_hashid
+from {{ ref('products_scd') }}
+-- products from {{ source('public', '_airbyte_raw_products') }}
+where 1 = 1
+and _airbyte_active_row = 1
+{{ incremental_clause('_airbyte_emitted_at', this) }}
+
