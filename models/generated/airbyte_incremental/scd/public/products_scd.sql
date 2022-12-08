a15cfd06fb152fa0f3269d5df74e77b47cd175d7
kalimlsn <kalimlsn@users.noreply.github.com>
2022-12-08 12:50:12 +0530

    Update products_scd.sql

--- models/generated/airbyte_incremental/scd/public/products_scd.sql
+++ models/generated/airbyte_incremental/scd/public/products_scd.sql
@@ -213,7 +213,7 @@ dedup_data as (
 select
     _airbyte_unique_key,
     _airbyte_unique_key_scd,
-   _links->self as id ,
+   _links -> self as id ,
     sku,
     {{ adapter.quote('name') }},
     slug,
