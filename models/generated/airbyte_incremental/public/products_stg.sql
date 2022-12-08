227f00c986e23897d90a18257416862774c0d595
kalimlsn <kalimlsn@users.noreply.github.com>
2022-12-08 11:19:54 +0530

    Update products_stg.sql

--- models/generated/airbyte_incremental/public/products_stg.sql
+++ models/generated/airbyte_incremental/public/products_stg.sql
@@ -8,7 +8,7 @@
 -- depends_on: {{ ref('products_ab2') }}
 select
     {{ dbt_utils.surrogate_key([
-      adapter.quote('id') ,
+      object_to_string('id') ,
         'sku',
         adapter.quote('name'),
         'slug',
